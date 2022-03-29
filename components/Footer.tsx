d8af83bc9c10d69cf4786144ae94ef3b6f8b6432
Vann x Professional <vannxprofessional@Yongs-MacBook-Pro.local>
2022-03-29 23:15:02 +0700

    Change style in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,38 +5,38 @@ function Footer() {
     <div className="text-gray-166 grid grid-cols-1 gap-y-10 bg-gray-100 px-32 py-14 md:grid-cols-4">
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works?</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <p className="cursor-pointer hover:underline">How Airbnb works?</p>
+        <p className="cursor-pointer hover:underline">Newsroom</p>
+        <p className="cursor-pointer hover:underline">Investors</p>
+        <p className="cursor-pointer hover:underline">Airbnb Plus</p>
+        <p className="cursor-pointer hover:underline">Airbnb Luxe</p>
       </div>
 
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">COMMUNITY</h5>
-        <p>Airbnb.org: disaster relief housing</p>
-        <p>Support Afghan refugees</p>
-        <p>Combating discrimination</p>
-        <p>Clone site</p>
+        <p className="cursor-pointer hover:underline">Airbnb.org: disaster relief housing</p>
+        <p className="cursor-pointer hover:underline">Support Afghan refugees</p>
+        <p className="cursor-pointer hover:underline">Combating discrimination</p>
+        <p className="cursor-pointer hover:underline">Clone site</p>
       </div>
 
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">HOST</h5>
-        <p>Clone Airbnb</p>
-        <p>Presents</p>
-        <p>Kirirom Institute of Technology</p>
-        <p>Software Engineer</p>
-        <p>Join Now</p>
+        <p className="cursor-pointer hover:underline">Clone Airbnb</p>
+        <p className="cursor-pointer hover:underline">Presents</p>
+        <p className="cursor-pointer hover:underline">Kirirom Institute of Technology</p>
+        <p className="cursor-pointer hover:underline">Software Engineer</p>
+        <p className="cursor-pointer hover:underline">Join Now</p>
       </div>
 
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold">SUPPORT</h5>
-        <p>Help Center</p>
-        <p>Safety information</p>
-        <p>Cancellation options</p>
-        <p>Our COVID-19 Response</p>
-        <p>Supporting people with disabilities</p>
-        <p>Report a neighborhood concern</p>
+        <p className="cursor-pointer hover:underline">Help Center</p>
+        <p className="cursor-pointer hover:underline">Safety information</p>
+        <p className="cursor-pointer hover:underline">Cancellation options</p>
+        <p className="cursor-pointer hover:underline">Our COVID-19 Response</p>
+        <p className="cursor-pointer hover:underline">Supporting people with disabilities</p>
+        <p className="cursor-pointer hover:underline">Report a neighborhood concern</p>
       </div>
 
       <div></div>
